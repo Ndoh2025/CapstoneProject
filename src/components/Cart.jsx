fa1881d1f9b1095d5dd3713396014f09ae5f033c
Ndoh2025 <ndohwilson@yahoo.com>
2023-09-25 16:26:14 -0500

    changes to cart component

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,40 +1,38 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useState } from 'react';
 
 function Cart() {
-  // Initialize state to store the list of carts
-  const [cart, setCarts] = useState({});
+  const [carts, setCarts] = useState([]);
 
-  // Use useEffect to make the API request when the component mounts
   useEffect(() => {
-    // Fetch the list of carts from the API
-    fetch("https://fakestoreapi.com/carts/user/2")
+    fetch('https://fakestoreapi.com/carts')
       .then((res) => res.json())
-      .then((json) => setCarts(json[0]))
-      .catch((error) => {
-        console.error("Error fetching data:", error);
-      });
-  }, []); // The empty dependency array ensures this effect runs only once
-console.log(cart)
+      .then((data) => setCarts(data))
+      .catch((error) => console.error('Error fetching carts:', error));
+  }, []);
+
   return (
     <div>
-      <h1>Cart List</h1>
+      <h1>Cart Component</h1>
       <ul>
+        {carts.map((cart) => (
           <li key={cart.id}>
-            <h2>Cart ID: {cart.id}</h2>
+            <h2>Cart #{cart.id}</h2>
             <p>User ID: {cart.userId}</p>
             <p>Date: {cart.date}</p>
             <h3>Products:</h3>
             <ul>
-               {cart.products.map((product) => (
+              {cart.products.map((product) => (
                 <li key={product.productId}>
                   Product ID: {product.productId}, Quantity: {product.quantity}
                 </li>
               ))}
             </ul>
           </li>
+        ))}
       </ul>
     </div>
   );
 }
 
 export default Cart;
+
